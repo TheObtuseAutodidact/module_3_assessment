fc026b8db0d87fb451c41643e61d2301e0f85f4f
Obtuse <theobtuseautodidact@gmail.com>
2016-04-26 11:30:56 -0600

    still building service

--- app/services/best_buy_service.rb
+++ app/services/best_buy_service.rb
@@ -6,4 +6,8 @@ class BestBuyService
     connection.params['apiKey'] = ENV['best_buy_key']
   end
 
+  def search(keyword)
+    connection.get("products((search=#{keyword})&salePrice<500&categoryPath.id=pcmcat209000050006)?show=name,sku,salePrice,shortDescription,customerReviewAverage,image&format=json")
+  # endsku, name, customer average review, short description, sale price, and image
+
 end
