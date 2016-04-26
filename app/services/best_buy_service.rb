be0c9bded86fc2e992621ff5de0324eb9d7aaaef
Obtuse <theobtuseautodidact@gmail.com>
2016-04-26 11:53:39 -0600

    getting response - incorrect item count

--- app/services/best_buy_service.rb
+++ app/services/best_buy_service.rb
@@ -1,13 +1,13 @@
 class BestBuyService
-  attr_reader :connection
+  attr_accessor :connection
 
   def initialize
-    @connection = Faraday.new("http://api.bestbuy.com/v1/products")
+    @connection = Faraday.new("http://api.bestbuy.com/v1")
     connection.params['apiKey'] = ENV['best_buy_key']
   end
 
   def search(keyword)
-    JSON.parse(connection.get("products((search=#{keyword})?show=name,sku,salePrice,shortDescription,customerReviewAverage,image&format=json").body)
+    JSON.parse(connection.get("products(search=#{keyword})?show=name,sku,salePrice,shortDescription,customerReviewAverage,image&pageSize=15&format=json").body)
   # endsku, name, customer average review, short description, sale price, and image
 end
 
